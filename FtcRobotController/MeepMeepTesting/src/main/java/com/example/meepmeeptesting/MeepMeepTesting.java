4ebf3c0399ee82e0d02df8d883cfbd0712b8a91c
Robosapiens-20 <ftcrobosapiens@gmail.com>
2024-09-29 14:46:22 -0500

    Update MeepMeepTesting.java

--- FtcRobotController/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ FtcRobotController/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,15 +14,10 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(35, 70, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(10)
-                .turn(Math.toRadians(90))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(35, 70, Math.toRadians(-90)))
+                .setTangent(-10)
+                .splineToLinearHeading(new Pose2d(0, 38, Math.toRadians(90)), Math.PI / 2)
+                        .lineToY(29)
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
