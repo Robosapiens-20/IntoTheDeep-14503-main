51f83c234434d06d92dcdc45bb96fe43cec3a9c9
Robosapiens-20 <ftcrobosapiens@gmail.com>
2024-10-04 17:15:52 -0500

    New Pathing File

--- FtcRobotController/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ FtcRobotController/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,7 +14,7 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-35, -70, Math.toRadians(90)))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(35, -70, Math.toRadians(90)))
                 .setTangent(-10)
                 .splineToSplineHeading(new Pose2d(0, -38, Math.toRadians(-90)), Math.PI / 2)
                 .lineToY(-29)
@@ -36,7 +36,6 @@ public class MeepMeepTesting {
                 .splineToSplineHeading(new Pose2d(-56,-56,Math.toRadians(50)),-Math.PI)
                 .lineToX(-66)
                 .build());
-
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedClip.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class AutoRedClip extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumDrive drive;
+        drive = new MecanumDrive(hardwareMap,new Pose2d(35,70,Math.toRadians(90)));
+// actionBuilder builds from the drive steps passed to it
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                .setTangent(-10)
+                .splineToSplineHeading(new Pose2d(0, -38, Math.toRadians(-90)), Math.PI / 2)
+                .lineToY(-29)
+                .lineToY(-38)
+                .setTangent(0)
+                .splineToSplineHeading(new Pose2d(51,-52,Math.toRadians(90)),0)
+
+                .setTangent(-60)
+                .splineToSplineHeading(new Pose2d(49,-36,Math.toRadians(90)),0)
+                .setTangent(-10)
+                .splineToSplineHeading(new Pose2d(56,-56,Math.toRadians(-90)),Math.PI)
+                .setTangent(-60)
+                .splineToSplineHeading(new Pose2d(58,-36,Math.toRadians(90)),0)
+                .setTangent(-10)
+                .splineToSplineHeading(new Pose2d(56,-56,Math.toRadians(-90)),Math.PI)
+                .setTangent(-60)
+                .splineToSplineHeading(new Pose2d(70,-36,Math.toRadians(90)),0)
+                .setTangent(-10)
+                .splineToSplineHeading(new Pose2d(56,-56,Math.toRadians(-90)),Math.PI)
+                .lineToX(-66)
+
+
+        waitForStart();
+        if (isStopRequested()) return;
+        Action trajectoryActionChosen;
+            trajectoryActionChosen = tab1.build();
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajectoryActionChosen,
+                )
+        );
+    }
+
+}
