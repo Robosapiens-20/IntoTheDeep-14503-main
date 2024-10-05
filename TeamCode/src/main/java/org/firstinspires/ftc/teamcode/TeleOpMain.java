88d0231a26025f1be4954a02a9ff07a7973ff51f
Robosapiens-20 <ftcrobosapiens@gmail.com>
2024-10-05 15:15:21 -0500

    Update TeleOpMain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -4,12 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "FirstTeleOp")
+@TeleOp(name = "IntakeTest")
 public class TeleOpMain extends LinearOpMode {
+    private DcMotor intake;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        intake = hardwareMap.dcMotor.get("intake");
 
         waitForStart();
+        while(opModeIsActive()){
+            intake.setPower(gamepad1.right_stick_y);
+        }
     }
 }
