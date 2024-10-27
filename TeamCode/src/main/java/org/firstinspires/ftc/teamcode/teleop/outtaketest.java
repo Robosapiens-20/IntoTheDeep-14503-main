2b3d60ad02d16916e924452f875f0834b6555c60
Robosapiens-20 <ftcrobosapiens@gmail.com>
2024-10-27 16:34:51 -0500

    updated outtake test and fine tuned outtake transfer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/outtaketest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/outtaketest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+//
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
