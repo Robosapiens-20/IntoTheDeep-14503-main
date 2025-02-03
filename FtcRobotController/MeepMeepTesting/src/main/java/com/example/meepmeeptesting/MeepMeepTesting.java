0618352c565d14cde2b9d08cc02437013c855ef6
Pranav Pulluru <143956301+PranavPulluru@users.noreply.github.com>
2025-02-02 23:40:04 -0600

    five clip for me to push to other real code

--- FtcRobotController/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ FtcRobotController/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,26 +14,40 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-35, -70, Math.toRadians(-90)))
-                .setTangent(-10)
-                .splineToSplineHeading(new Pose2d(0, -34, Math.toRadians(-90)), Math.PI / 2)
-                .lineToY(-34)
-                .setTangent(0)
-                .splineToSplineHeading(new Pose2d(-49,-42,Math.toRadians(90)),0)
-                .waitSeconds(1)
-                //extend intake and intake first block 100%
-                .splineToSplineHeading(new Pose2d(-56,-56,Math.toRadians(50)),Math.PI)
-                //deposit first block
-                .turnTo(Math.toRadians(95))
-                .waitSeconds(1)
-                //extend intake and intake second block fr
-                .turnTo(Math.toRadians(49))
-                //deposit second block
-                .turnTo(Math.toRadians(120))
-                .waitSeconds(1)
-                //extend and intake the last block
-                .turnTo(Math.toRadians(49))
-                //deposit last block free ahh auto
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(4, -62.5, Math.toRadians(90)))
+                        .setTangent(Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(4,-32.5, Math.toRadians(90)), Math.toRadians(90))//First deposit
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(35,-39, Math.toRadians(65)), Math.toRadians(360))//Positioning spline
+                        //extend linkage
+                        .turnTo(Math.toRadians(330))
+                        //first push
+                        .setTangent(Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(43,-39, Math.toRadians(65)), Math.toRadians(0))
+                        .turnTo(Math.toRadians(330))
+                        //second push
+                        .setTangent(Math.toRadians(0))
+                        .splineToLinearHeading(new Pose2d(53,-39, Math.toRadians(65)), Math.toRadians(0))
+                        .turnTo(Math.toRadians(330))
+                        //third push
+                        .setTangent(Math.toRadians(180))
+                        //retract linkage
+                        .splineToLinearHeading(new Pose2d(45,-52,Math.toRadians(90)), Math.toRadians(-90))
+                        //second block pick
+                        .setTangent(Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(1,-32.5,Math.toRadians(90)), Math.toRadians(90))
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(45,-52,Math.toRadians(90)), Math.toRadians(-90))
+                        .setTangent(Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(-2,-32.5, Math.toRadians(90)), Math.toRadians(90))
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(45,-52,Math.toRadians(90)), Math.toRadians(-90))
+                        .setTangent(Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(-5,-32.5,Math.toRadians(90)), Math.toRadians(90))
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(45,-52,Math.toRadians(90)), Math.toRadians(-90))
+                        .setTangent(Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(6,-32.5, Math.toRadians(90)), Math.toRadians(90))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
